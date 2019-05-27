1c7cff6a9e595370d23dfa1f165ad31c36191922
Alidar40 <alidar.asvarov@gmail.com>
2019-05-27 16:29:42 +0300

    [add] Pricelist editing
    Now client can insert/update/delete pricelist items

--- client/react/src/components/pricelist.js
+++ client/react/src/components/pricelist.js
@@ -1,91 +1,188 @@
 import React from 'react';
 import Cookies from 'js-cookie';
 import { TacoTable, DataType, SortDirection, Formatters, Summarizers, TdClassNames } from 'react-taco-table';
+import ReactTable from "react-table";
+import "react-table/react-table.css";
 
 import { formatJsonDateToUTC } from '../utils/date';
 
 class Pricelist extends React.Component {
     constructor(props) {
-        super(props);
+	super(props);
     	this.companyId = Cookies.get('companyId')
-        this.state = {
-            pricelist: null,
-            pricelistFetched: false,
-        }
+	this.state = {
+	    pricelist: null,
+	    pricelistFetched: false,
+	}
+	this.renderEditable = this.renderEditable.bind(this);
+	this.handleAddItemClick = this.handleAddItemClick.bind(this);
+	this.handleUpdateClick = this.handleUpdateClick.bind(this);
+	this.request = {
+		insert: [],
+		update: [],
+		delete: []
+	}
 
-        fetch('/api/supplier/pricelist/' + this.companyId, {
-            method: 'GET',
-            /*headers: {
-                'Set-Cookie': Cookies.get('.AspNetCore.Identity.Application'),
-            }*/
-        })
-            .then(response => {
-                return response.json();
-            })
-            .then(data => {
-                if (JSON.stringify(data) === JSON.stringify([])) {
-                    this.setState({ ordersHistory: "PRICELIST_IS_EMPTY", historyFetched: true });
-                } else {
-                    this.setState({ pricelist: data, pricelistFetched: true });
-                }
-            })
-            .catch(error => {
-                console.log(error);
-            })
+	fetch('/api/supplier/pricelist/' + this.companyId, {
+		method: 'GET',
+	})
+	.then(response => {
+		return response.json();
+	})
+	.then(data => {
+		if (JSON.stringify(data) === JSON.stringify([])) {
+			this.setState({ ordersHistory: "PRICELIST_IS_EMPTY", historyFetched: true });
+		} else {
+			this.setState({ pricelist: data, pricelistFetched: true });
+		}
+	})
+	.catch(error => {
+		console.log(error);
+	})
     }
 
     componentWillUnmount() {
         this.setState({ pricelistFetched: false });
     }
 
-    Pricelist(pricelist, columns) {
-        if (pricelist === "PRICELIST_IS_EMPTY") {
-            return <div></div>
-        }
-        return <div>
-            <h2>My pricelist</h2>
-            <TacoTable
-                className="table table-hover simple-example table-full-width table-striped table-sortable"
-                columns={columns}
-                columnHighlighting
-                data={pricelist}
-                striped
-                sortable
-            />
-        </div>
+    renderEditable(cellInfo) {
+    	return (
+      		<div
+			style={{ backgroundColor: "#fafafa" }}
+			contentEditable
+			suppressContentEditableWarning
+			onBlur={e => {
+			  const data = [...this.state.pricelist];
+			  if (data[cellInfo.index][cellInfo.column.id] != e.target.innerHTML) {
+				  if (data[cellInfo.index].id === "") {
+					  var index = this.request.insert.indexOf(data[cellInfo.index])
+					  if (index >= 0) {
+						this.state.request.insert.splice(index, 1)
+					  }
+					  if (cellInfo.column.id == "price") {
+						var t = parseFloat(e.target.innerHTML)
+						if (t !== NaN) {
+							data[cellInfo.index][cellInfo.column.id] = t
+						}
+					  } else {
+					  	data[cellInfo.index][cellInfo.column.id] = e.target.innerHTML;
+					  }
+					  this.request.insert.push(data[cellInfo.index])
+				  } else {
+					  var index = this.request.update.indexOf(data[cellInfo.index])
+					  if (index >= 0) {
+						this.state.request.update.splice(index, 1)
+					  }
+					  if (cellInfo.column.id == "price") {
+						var t = parseFloat(e.target.innerHTML)
+						if (t !== NaN) {
+							data[cellInfo.index][cellInfo.column.id] = t
+						}
+					  } else {
+					  	data[cellInfo.index][cellInfo.column.id] = e.target.innerHTML;
+					  }
+					  this.request.update.push(data[cellInfo.index])
+				  }
+				  this.setState({
+					pricelist: data,
+					request: this.request
+				  });
+			  }
+			}}
+			dangerouslySetInnerHTML={{
+			  __html: this.state.pricelist[cellInfo.index][cellInfo.column.id]
+			}}
+	      	/>
+	    );
     }
 
-    render() {
-        if (this.state.pricelistFetched) {
-            const columns = [
-                {
-                    id: 'sku',
-                    type: DataType.String,
-                    header: 'Sku',
-                },
-                {
-                    id: 'name',
-                    type: DataType.String,
-                    header: 'Name',
-                },
-                {
-                    id: 'units', type: DataType.String,
-                    header: 'Units',
-                },
-                {
-                    id: 'price',
-                    type: DataType.NumberOrdinal,
-                    header: 'Price',
-                }];
-
-            return <div className="container body-content">
-                {this.Pricelist(this.state.pricelist, columns)}
-
-            </div>
-        } else {
-            return <div className="container body-content"><br /><h3>Loading content</h3></div>
-        }
+    handleAddItemClick() {
+		this.state.pricelist.push({id: "", sku:"_", name: "_", units: "_", price: 0})
+		this.setState({pricelist: this.state.pricelist})
     }
+
+    handleUpdateClick() {
+        fetch('/api/supplier/pricelist/edit', {
+            method: 'PUT',
+	    headers: {
+	    	'Content-Type': 'application/json',
+	    },
+	    body: JSON.stringify(this.request)
+        })
+            .then(response => {
+	    	if (response.status == 200) {
+			this.forceUpdate()
+		}
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
+	render() {
+	    if (this.state.pricelistFetched) {
+		    const data = this.state.pricelist;
+		    return (
+		      	<div>
+				<ReactTable
+				  data={data}
+				  columns={[
+				    {
+				      Header: "Sku",
+				      accessor: "sku",
+				      Cell: this.renderEditable
+				    },
+				    {
+				      Header: "Name",
+				      accessor: "name",
+				      Cell: this.renderEditable
+				    },
+				    {
+				      Header: "Units",
+				      accessor: "units",
+				      Cell: this.renderEditable
+				    },
+				    {
+				      Header: "Price",
+				      accessor: "price",
+				      Cell: this.renderEditable
+				    },
+				    {
+				      Header: "Delete",
+				      id: "delete",
+				      accessor: d =>
+					<button
+					  dangerouslySetInnerHTML={{
+					    __html: "delete"
+					  }}
+					  onClick={() => {
+						if (d.id === "") {
+							var index = this.request.insert.indexOf(d)
+							this.request.insert.splice(index, 1)
+						} else {
+							this.request.delete.push({ id: d.id})
+						}
+						var index = this.state.pricelist.indexOf(d)
+						this.state.pricelist.splice(index, 1)
+						this.setState({
+							pricelist: this.state.pricelist,
+							request: this.request
+						});
+					  }}
+					></button>
+				    }
+				  ]}
+				  defaultPageSize={10}
+				  className="-striped -highlight"
+				/>
+			    <button onClick={this.handleUpdateClick}>Update pricelist</button>
+			    <button onClick={this.handleAddItemClick}>Add item</button>
+			    </div>
+			    );
+		  } else {
+		    return <div className="container body-content"><br /><h3>Loading content</h3></div>
+	  	}
+	}
 }
 
 export default Pricelist;
